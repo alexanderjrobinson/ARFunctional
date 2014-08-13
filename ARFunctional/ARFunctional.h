7ebfee2c15fc376b0be096245f1a7b6526e5bf07
Alex Robinson <alex@madebyatomicrobot.com>
2014-08-13 17:33:36 -0400

    Initial Commit

---
+++ ARFunctional/ARFunctional.h
@@ -0,0 +1,13 @@
+//
+//  ARFunctional.h
+//  ARFunctional
+//
+//  Created by Alex Robinson on 8/13/14.
+//  Copyright (c) 2014 Alex Robinson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ARFunctional : NSObject
+
+@end
---
+++ ARFunctional/ARFunctional.m
@@ -0,0 +1,13 @@
+//
+//  ARFunctional.m
+//  ARFunctional
+//
+//  Created by Alex Robinson on 8/13/14.
+//  Copyright (c) 2014 Alex Robinson. All rights reserved.
+//
+
+#import "ARFunctional.h"
+
+@implementation ARFunctional
+
+@end
